3b98c1824ed80e9009171953dc948de92d1870e1
cc-visionary <lim.christopher.go@gmail.com>
2022-11-18 10:57:16 +0800

    Back to utilizing compass heading of device

--- frontend/src/InputForm.js
+++ frontend/src/InputForm.js
@@ -60,7 +60,7 @@ const InputForm = () => {
     });
   };
 
-  const requestDeviceOrientation = () => {
+  const requestMagnetometer = () => {
     // start compass
     if (deviceType === "Mobile") {
       DeviceOrientationEvent.requestPermission()
@@ -88,41 +88,11 @@ const InputForm = () => {
   };
 
   const handleOrientation = (event) => {
-    const { alpha, beta, gamma } = event;
-
-    // Convert degrees to radians
-    var alphaRad = alpha * (Math.PI / 180);
-    var betaRad = beta * (Math.PI / 180);
-    var gammaRad = gamma * (Math.PI / 180);
-
-    // Calculate equation components
-    var cA = Math.cos(alphaRad);
-    var sA = Math.sin(alphaRad);
-    var cB = Math.cos(betaRad);
-    var sB = Math.sin(betaRad);
-    var cG = Math.cos(gammaRad);
-    var sG = Math.sin(gammaRad);
-
-    // Calculate A, B, C rotation components
-    var rA = -cA * sG - sA * sB * cG;
-    var rB = -sA * sG + cA * sB * cG;
-    var rC = -cB * cG;
-
-    // Calculate compass heading
-    var compassHeading = Math.atan(rA / rB);
-
-    // Convert from half unit circle to whole unit circle
-    if (rB < 0) {
-      compassHeading += Math.PI;
-    } else if (rA < 0) {
-      compassHeading += 2 * Math.PI;
-    }
-
-    // Convert radians to degrees
-    compassHeading *= 180 / Math.PI;
+    const { alpha } = event;
+    const compass = event.webkitCompassHeading || Math.abs(alpha - 360);
 
     setDeviceOrientationPermission(true);
-    setCompass(compassHeading);
+    setCompassValue(compass);
   };
 
   const getDevice = () => {
@@ -315,10 +285,7 @@ const InputForm = () => {
           </div>
           <div>
             ({deviceOrientationPermission ? "/" : "X"}){" "}
-            <span
-              className="clickable"
-              onClick={() => requestDeviceOrientation()}
-            >
+            <span className="clickable" onClick={() => requestMagnetometer()}>
               Magnetometer
             </span>
           </div>
